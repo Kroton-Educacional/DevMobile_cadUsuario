7c950b02264367c91b8fe06c7fb03f5cb7a842e0
Leonardo Rocha <leonardo@leonardorocha.eti.br>
2021-06-08 10:04:34 -0300

    Novos arquivos para criação de base de dados

--- app/src/main/java/com/example/cadastrocliente/CreateUser.java
+++ app/src/main/java/com/example/cadastrocliente/CreateUser.java
@@ -2,15 +2,33 @@ package com.example.cadastrocliente;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateUser extends AppCompatActivity {
 
+    EditText nomecompleto;
+    EditText email;
+    Button salvar;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_user);
+
+        nomecompleto = findViewById(R.id.nome_completo);
+        email = findViewById(R.id.email);
+        salvar = findViewById(R.id.btnSalvar);
+
+        salvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cadastrocliente/MainActivity.java
+++ app/src/main/java/com/example/cadastrocliente/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.cadastrocliente;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,12 +12,20 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
     Button btnCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_view);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new UserAdapter();
+        recyclerView.setAdapter(adapter);
 
         btnCadastrar = findViewById(R.id.btnCadastrar);
 
